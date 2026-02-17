7d9294256d159129493e1476c26afbbcf9033dc6
Shubham kusale <98016063+shubhamkusale@users.noreply.github.com>
2026-02-17 19:11:17 +0530

    Jarvis - Week 1

---
+++ WEEK1/jarvis_day1.py
@@ -0,0 +1,9 @@
+# JARVIS - Week 1, Day 1
+# Today Jarvis is just a print statement
+# In 32 weeks it will have a voice
+
+def jarvis(command):
+    print(f"Processing: {command}")
+    return "Jarvis online. Learning in progress."
+
+print(jarvis("Hello Jarvis"))
